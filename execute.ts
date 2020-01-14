48122e66b49969eb4e8d9c3f2775291e86348b3e
Anthony Martin <antm88@gmail.com>
2020-01-13 23:19:21 -0800

    Add ability to run in interactive mode

---
+++ execute.ts
@@ -0,0 +1,14 @@
+import { series } from 'async';
+import path = require('path');
+import { Template, renderTemplate } from './src/template';
+
+series([async () => {
+  const execPath = path.resolve(process.argv[2]);
+  const execute = (await import(execPath)).default;
+  
+  const template = new Template();
+  execute(template);
+  
+  const generated = renderTemplate(template);
+  console.log(JSON.stringify(generated, null, 2));
+}]);
